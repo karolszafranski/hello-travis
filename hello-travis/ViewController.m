4efae6369d7983b107d178374d33d6ecde214536
Karol Szafrański <kszafranski@eclipsesource.com>
2017-11-18 15:16:39 +0100

    Display IP address in the app

--- hello-travis/ViewController.m
+++ hello-travis/ViewController.m
@@ -7,11 +7,14 @@
 //
 
 #import "ViewController.h"
+#import <MyIPAddressKit/MyIpAddressKit.h>
 
 @interface ViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel* helloWorldLabel;
 @property (weak, nonatomic) IBOutlet UILabel* currentIpAddressLabel;
+
+@property (strong, nonatomic) IPAddressRequest* ipAddressRequest;
 
 @end
 
@@ -24,6 +27,20 @@
     self.helloWorldLabel.text = @"Hello Travis";
     self.currentIpAddressLabel.text = @"0.0.0.0";
     self.currentIpAddressLabel.font = [UIFont italicSystemFontOfSize:[UIFont smallSystemFontSize]];
+
+    self.ipAddressRequest = [[IPAddressRequest alloc] init];
+    self.ipAddressRequest.URLSession = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    __weak typeof(self) weakSelf = self;
+    [self.ipAddressRequest requestWithCompletionHandler:^(NSString * IPAddressString, NSError * error) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (error) {
+                NSLog(@"%@", error);
+            }
+            else {
+                weakSelf.currentIpAddressLabel.text = IPAddressString;
+            }
+        });
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
